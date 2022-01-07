a56c09f816d8b0f66f5326f41e41134078452965
bofu <bofusoci@gmail.com>
2022-01-07 15:31:47 +0800

    新增資料庫api測試

--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
+const path = require('path');
+const cors = require('cors');
 const express = require('express');
-const app = express();
-const port = 7000; // your server port, you can change it yourself
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const app = express(); // create express app
+app.use(cors());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// add middlewares
+const root = require('path').join('../static', 'build');
+app.use(express.static(root));
+
+app.use('/*', (req, res) => {
+  res.sendFile(path.join('../static', 'build', 'index.html'));
 });
-app.listen(port, () => {
-  console.log(`RUN http://localhost:${port}`)
-});
+
+// start express server on port 5000
+app.listen(process.env.PORT || 5000, () => {
+  console.log('server started');
+}); 
+
