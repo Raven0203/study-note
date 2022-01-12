a62c2d7fd5ee2b90fd8c7f062cf921fec80fabb1
mikeHung <mikehung425@gmail.com>
2022-01-12 22:25:39 +0800

    post/put button & part of delete event

--- src/components/JourneyPlanning/PlanTableTest.jsx
+++ src/components/JourneyPlanning/PlanTableTest.jsx
@@ -1,20 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import { DirectionsService, LoadScript } from '@react-google-maps/api';
 var temp = [];
-var title;
+var beginDate;
 var respone;
 var jsondata;
+var post=true;//if true click button to post data else,put data 
+var test="";
+
 function PlanTableTest({setResault,place}) {
+    let element = document.getElementsByClassName("delbutton");
+    for(let l =0;l<element.length;l++){
+        element[l].addEventListener("click", function test(){
+            jsondata = JSON.parse(window.localStorage.jsondata)
+            jsondata.journeydetail = JSON.parse(jsondata.journeydetail)
+            jsondata.journeydetail.eachDays[0].eachPlaces.splice(parseInt(this.id.substr(6,1)),1);
+            jsondata.journeydetail= JSON.stringify(jsondata.journeydetail)
+            window.localStorage.jsondata= JSON.stringify(jsondata);
+            dataparse(jsondata)
+        })
+    }
     const [data, setData] = useState([])
      console.log("LOAD")
-     if(window.localStorage.jsondata){
+     function FetchData(journeyid) {
+         //get資料庫已有行程
+        fetch("http://localhost:8080/journey/"+journeyid)
+            .then((res) => {
+               return res.json();
+            }).then((jsondata) => {
+                dataparse(jsondata)
+                console.log(jsondata)
+            })
+    }
+     if(window.localStorage.jsondata/*檢查local storge*/ ){
         jsondata = JSON.parse(window.localStorage.jsondata)
         console.log("------------------------")
         jsondata.journeydetail = JSON.parse(jsondata.journeydetail)
         let insert=true;
-        for(let i =0;i<jsondata.journeydetail.eachDays[0].eachPlaces.length;i++){
-            
+        for(let i =0;i<jsondata.journeydetail.eachDays[0].eachPlaces.length;i++){            
              if(jsondata.journeydetail.eachDays[0].eachPlaces[i].AttractionsId==place.place_id){
+                 //判斷 prop不是重複資料
                 insert=false;
                 break;
                             }
@@ -24,15 +48,12 @@ function PlanTableTest({setResault,place}) {
             jsondata.journeydetail= JSON.stringify(jsondata.journeydetail)
             window.localStorage.jsondata= JSON.stringify(jsondata);
             dataparse(jsondata)
- 
-
         }       
        
-    }else{
+    }else{//如果local storge 沒東西放一個 journey detail 格式的json進去
        let josonmodel ={};
-     //  console.log(josonmodel)
         josonmodel.journeydetail={}
-        josonmodel.journeydetail.beginDate="2021/02/15"
+        josonmodel.journeydetail.beginDate="2021-02-15"
         josonmodel.journeydetail.daysNum=2
         josonmodel.journeydetail.eachDays=[];
        
@@ -40,16 +61,8 @@ function PlanTableTest({setResault,place}) {
         placeobject.beginTime = "0900"
         placeobject.placesNum = "2"
         placeobject.eachPlaces=[];
-        //placeobject.eachPlaces.push({"placeName":place.name,"AttractionsId":"123"})
         josonmodel.journeydetail.eachDays.push(placeobject)
-        josonmodel.journeydetail= JSON.stringify(josonmodel.journeydetail)
-        // if(placeobject!=={}){
-        //     alert(123)
-        //     josonmodel.journeydetail.eachDays.push(placeobject)
-        // }
-
-        //jsondata = josonmodel;
-        //dataparse(jsondata);
+        josonmodel.journeydetail= JSON.stringify(josonmodel.journeydetail)//為了要journeybean 對照到detail是string
         window.localStorage.jsondata= JSON.stringify(josonmodel);
     }
     useEffect(()=>{
@@ -59,21 +72,20 @@ function PlanTableTest({setResault,place}) {
 
 
     function dataparse(jsondata) {
-        //alert(typeof jsondata.journeydetail.eachDays[0])
-        //temp = jsondata.journeydetail.eachDays[0].eachPlaces;
-        console.log("-----dataparse------")
-        console.log(temp)
-        console.log("-------------------")
-        //title = jsondata.journeydetail.beginDate;
-        temp = JSON.parse(jsondata.journeydetail).eachDays[0].eachPlaces;
-        title = JSON.parse(jsondata.journeydetail).beginDate;
+        temp = JSON.parse(jsondata.journeydetail).eachDays[0].eachPlaces; //暫存區放eachplace array
+        // if(temp.length===1){
+        //     test = temp[0].placeName
+        //     setData(temp);
+        // }
+        beginDate = JSON.parse(jsondata.journeydetail).beginDate;
         let count = 0;
         for (var i = 0; i < temp.length - 1; i++) {
-            let continute = FetchDistance(temp[i].AttractionsId, temp[i + 1].AttractionsId, temp[i]);
+            test="";
+            let continute = FetchDistance(temp[i].AttractionsId, temp[i + 1].AttractionsId, temp[i]);//取陣列送資料給derection算距離
             continute.then(() => {
                 count++;
                 if (count == temp.length - 1) {
-                    setData(temp);
+                    setData(temp);                   
                     console.log("DRAW TABLE")
                 }
             });
@@ -91,7 +103,7 @@ function PlanTableTest({setResault,place}) {
                 result.distanceValue = data.distance.value;
                 result.durationText = data.duration.text;
                 result.durationValue = data.duration.value;
-                temp.distance = `距離:${result.distanceText} 時間:${result.durationText}`;
+                temp.distance = `距離:${result.distanceText} 時間:${result.durationText}`;//距離結果存回temp方便畫plan table
                 return true;
             })
     }
@@ -107,18 +119,50 @@ function PlanTableTest({setResault,place}) {
         console.log(waypoit);
         return waypoit;
     }
-    function saveData() {
-        fetch("http://localhost:8080/journey/2", {
-            method: 'POST', 
-            body: window.localStorage.jsondata, 
-            headers: new Headers({
-              'Content-Type': 'application/json'
-            })
-          }).then((res )=>{console.log(res)})
-          .catch(error => alert(error) )
-          .then(response => alert('Success:', response));
-    }
 
+
+    function saveData() {
+        if(post){
+            fetch("http://localhost:8080/journey/2", {
+                method: 'POST', 
+                body: window.localStorage.jsondata, 
+                headers: new Headers({
+                  'Content-Type': 'application/json'
+                })
+              }).then((res )=>{  return res.json() })
+                
+              .catch(error => alert(error) )
+              .then(response =>{          
+                FetchData(response.journeyid) //post後從資料庫拿新資料 轉換成編輯已有行程狀態
+                post = false;
+              });
+        }else{
+            fetch("http://localhost:8080/journey/", {//update
+                method: 'PUT', 
+                body: window.localStorage.jsondata, 
+                headers: new Headers({
+                  'Content-Type': 'application/json'
+                })
+              }).then((res )=>{  return res.json() })
+                
+              .catch(error => console.log(error) )
+              .then(response =>{
+                console.log(response)
+              });
+        }
+
+    }
+    
+    function deleteItem(index){
+        
+        jsondata = JSON.parse(window.localStorage.jsondata)
+        jsondata.journeydetail = JSON.parse(jsondata.journeydetail)
+        jsondata.journeydetail.eachDays[0].eachPlaces.splice(index,1)
+        jsondata.journeydetail= JSON.stringify(jsondata.journeydetail)
+        window.localStorage.jsondata= JSON.stringify(jsondata);
+        dataparse(jsondata)
+
+    }
 
     function setMap() {
         setResault(respone)
@@ -140,61 +184,15 @@ function PlanTableTest({setResault,place}) {
     }
         <table >
             <tr>
-                <th><input type="date" value={title} ></input></th>
-            </tr>{data.map((item) => {
-                return <><tr><td><b>{item.placeName}</b></td></tr><tr>{item.distance}</tr></>
+                <th><input type="date" value={beginDate} ></input></th>
+            </tr>
+            {(test=="")?"":<tr><b>{test}</b><button id={`delbtn0`} className='delbutton'>刪除</button></tr>}
+            
+            {data.map((item,idex) => {
+                return <><tr><td><b>{item.placeName}</b><button id={`delbtn${idex}`} className='delbutton'>刪除</button></td></tr><tr>{item.distance}</tr></>
             })}</table><button onClick={setMap}>MAP</button><br/><button onClick={saveData}>儲存</button>
     </div>)
 }
 
 export default PlanTableTest;
 
-
-// {
-//     "beginDate": "2021/02/15",
-//         "daysNum": 2,
-//             "eachDays": [
-//                 {
-//                     "beginTime": "0900",
-//                     "placesNum": "2",
-//                     "eachPlaces": [
-//                         {
-//                             "placeName": "臺中國家歌劇院 ",                              "AttractionsId":"ChIJcaKjTOw9aTQR4mAeJzejtdg",
-//                             "note": "老闆會拿棍子扁人，要小心"
-//                         },
-//                         {
-//                             "placeName": "國立自然科學博物館",
-//                             "AttractionsId": "ChIJyzSRBng9aTQRNk-Xj7pbpvc",
-//                             "note": "記得買土產"
-//                         },
-//                         {
-//                             "placeName": "綠光計畫范特喜文創聚落",
-//                             "AttractionsId": "ChIJO-oeCZ89aTQR5Y9uRU4uSEI",
-//                             "note": "老闆會拿棍子扁人，要小心"
-//                         },
-//                         {
-//                             "placeName": "國立臺灣美術館",
-//                             "AttractionsId": "ChIJyVHkNqY9aTQRDilsyqDVfbg",
-//                             "note": "老闆會拿棍子扁人，要小心"
-//                         },
-//                         {
-//                             "placeName": "國美館綠園道",
-//                             "AttractionsId": "ChIJW_5f_KY9aTQRlphcLmWj88I",
-//                             "note": "老闆會拿棍子扁人，要小心"
-//                         }
-
-
-//                     ]
-//                 },
-//                 {
-//                     "beginTime": "1400",
-//                     "placesNum": "1",
-//                     "eachPlaces": [{
-//                         "placeName": "休息站",
-//                         "stayTime": "30",
-//                         "note": "上車睡覺，下車尿尿"
-//                     }
-//                     ]
-//                 }
-//             ]
-// }
