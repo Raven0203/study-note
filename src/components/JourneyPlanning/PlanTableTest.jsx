96dfcd8f87f7c672937a2ca5130e90a447d46c95
mikeHung <mikehung425@gmail.com>
2022-01-13 14:28:21 +0800

    delete

--- src/components/JourneyPlanning/PlanTableTest.jsx
+++ src/components/JourneyPlanning/PlanTableTest.jsx
@@ -5,20 +5,9 @@ var beginDate;
 var respone;
 var jsondata;
 var post=true;//if true click button to post data else,put data 
-var test="";
-
+var singleTitle="";//one item event
+var lastplace;
 function PlanTableTest({setResault,place}) {
-    let element = document.getElementsByClassName("delbutton");
-    for(let l =0;l<element.length;l++){
-        element[l].addEventListener("click", function test(){
-            jsondata = JSON.parse(window.localStorage.jsondata)
-            jsondata.journeydetail = JSON.parse(jsondata.journeydetail)
-            jsondata.journeydetail.eachDays[0].eachPlaces.splice(parseInt(this.id.substr(6,1)),1);
-            jsondata.journeydetail= JSON.stringify(jsondata.journeydetail)
-            window.localStorage.jsondata= JSON.stringify(jsondata);
-            dataparse(jsondata)
-        })
-    }
     const [data, setData] = useState([])
      console.log("LOAD")
      function FetchData(journeyid) {
@@ -29,24 +18,18 @@ function PlanTableTest({setResault,place}) {
             }).then((jsondata) => {
                 dataparse(jsondata)
                 console.log(jsondata)
+                window.localStorage.jsondata = JSON.stringify(jsondata)
             })
     }
      if(window.localStorage.jsondata/*檢查local storge*/ ){
         jsondata = JSON.parse(window.localStorage.jsondata)
         console.log("------------------------")
         jsondata.journeydetail = JSON.parse(jsondata.journeydetail)
-        let insert=true;
-        for(let i =0;i<jsondata.journeydetail.eachDays[0].eachPlaces.length;i++){            
-             if(jsondata.journeydetail.eachDays[0].eachPlaces[i].AttractionsId==place.place_id){
-                 //判斷 prop不是重複資料
-                insert=false;
-                break;
-                            }
-        }
-        if(insert &&  place.place_id){
+        if(lastplace!=place.place_id &&  place.place_id ){
+            lastplace=place.place_id;
             jsondata.journeydetail.eachDays[0].eachPlaces.push({"placeName":place.place_name,"AttractionsId":place.place_id})
-            jsondata.journeydetail= JSON.stringify(jsondata.journeydetail)
-            window.localStorage.jsondata= JSON.stringify(jsondata);
+            localStore(jsondata);
+            console.log("insert")
             dataparse(jsondata)
         }       
        
@@ -62,29 +45,32 @@ function PlanTableTest({setResault,place}) {
         placeobject.placesNum = "2"
         placeobject.eachPlaces=[];
         josonmodel.journeydetail.eachDays.push(placeobject)
-        josonmodel.journeydetail= JSON.stringify(josonmodel.journeydetail)//為了要journeybean 對照到detail是string
-        window.localStorage.jsondata= JSON.stringify(josonmodel);
+        localStore(josonmodel);
     }
     useEffect(()=>{
         //dataparse(jsondata)
     },[])
     
-
+    function localStore(jsondata){
+        jsondata.journeydetail= JSON.stringify(jsondata.journeydetail)//為了要journeybean 對照到detail是string
+        window.localStorage.jsondata= JSON.stringify(jsondata);
+    }
 
     function dataparse(jsondata) {
         temp = JSON.parse(jsondata.journeydetail).eachDays[0].eachPlaces; //暫存區放eachplace array
-        // if(temp.length===1){
-        //     test = temp[0].placeName
-        //     setData(temp);
-        // }
+        if(temp.length===1){
+            singleTitle = temp[0].placeName
+            setData(temp); //只有一個物件時繪製journetplan用
+        }
         beginDate = JSON.parse(jsondata.journeydetail).beginDate;
         let count = 0;
         for (var i = 0; i < temp.length - 1; i++) {
-            test="";
+            singleTitle="";
             let continute = FetchDistance(temp[i].AttractionsId, temp[i + 1].AttractionsId, temp[i]);//取陣列送資料給derection算距離
             continute.then(() => {
                 count++;
                 if (count == temp.length - 1) {
+                   
                     setData(temp);                   
                     console.log("DRAW TABLE")
                 }
@@ -143,8 +129,7 @@ function PlanTableTest({setResault,place}) {
                 headers: new Headers({
                   'Content-Type': 'application/json'
                 })
-              }).then((res )=>{  return res.json() })
-                
+              }).then((res )=>{  return res.json() })               
               .catch(error => console.log(error) )
               .then(response =>{
                 console.log(response)
@@ -153,15 +138,13 @@ function PlanTableTest({setResault,place}) {
 
     }
     
-    function deleteItem(index){
-        
+    function deleteItem(e){ //delete event      
         jsondata = JSON.parse(window.localStorage.jsondata)
         jsondata.journeydetail = JSON.parse(jsondata.journeydetail)
-        jsondata.journeydetail.eachDays[0].eachPlaces.splice(index,1)
+        jsondata.journeydetail.eachDays[0].eachPlaces.splice(parseInt(e.target.id.substr(6,1)),1);
         jsondata.journeydetail= JSON.stringify(jsondata.journeydetail)
         window.localStorage.jsondata= JSON.stringify(jsondata);
         dataparse(jsondata)
-
     }
 
     function setMap() {
@@ -186,10 +169,10 @@ function PlanTableTest({setResault,place}) {
             <tr>
                 <th><input type="date" value={beginDate} ></input></th>
             </tr>
-            {(test=="")?"":<tr><b>{test}</b><button id={`delbtn0`} className='delbutton'>刪除</button></tr>}
+            {(singleTitle=="")?"":<tr><b>{singleTitle}</b><button id={`delbtn0`} className='delbutton'>刪除</button></tr>}
             
             {data.map((item,idex) => {
-                return <><tr><td><b>{item.placeName}</b><button id={`delbtn${idex}`} className='delbutton'>刪除</button></td></tr><tr>{item.distance}</tr></>
+                return <><tr><td><b>{item.placeName}</b><button id={`delbtn${idex}`} className='delbutton' onClick={deleteItem}>刪除</button></td></tr><tr>{item.distance}</tr></>
             })}</table><button onClick={setMap}>MAP</button><br/><button onClick={saveData}>儲存</button>
     </div>)
 }
